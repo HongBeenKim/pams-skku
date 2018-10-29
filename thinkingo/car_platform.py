3672347fb12bc5bf08c8c9ab7839a6afd6a56356
unknown <elpion19@gmail.com>
2018-10-30 00:41:38 +0900

    :rotating_light: need to test

--- thinkingo/car_platform.py
+++ thinkingo/car_platform.py
@@ -1,11 +1,55 @@
-# 그냥 다시 짜는 게 좋겠군.
+import serial
 from subroutine import Subroutine
 from data_class import Data
 
+
 class CarPlatform(Subroutine):
-    def __init__(self, data: Data):
-        pass
+    def __init__(self, port: str, data: Data):
+        super().__init__(data)
+        try:
+            self.serial = serial.Serial(port, 115200)
+        except Exception as e:
+            print("car_platform INIT ERROR: ", e)
 
     def main(self):
-        pass
+        while True:
+            self.receive()
+            self.send()
 
+    def send(self):
+        self.data.write_packet.alive = self.data.read_packet.alive
+        try:
+            self.serial.write(self.data.write_packet.write_bytes())
+        except Exception as e:
+            print("car_platform SEND ERROR: ", e)
+
+    def receive(self):
+        try:
+            binary_message = self.serial.read(18)
+        except Exception as e:
+            print("car_platform RESEIVE ERROR: ", e)
+            return
+        self.data.read_packet.read_bytes(binary_message)
+
+    def read(self):
+        return self.data.read_packet.speed, self.data.read_packet.enc
+
+    def write(self, gear, speed, steer, brake):
+        self.data.write_packet.gear = gear
+        self.data.write_packet.speed = speed
+        self.data.write_packet.steer = steer
+        self.data.write_packet.brake = brake
+
+    def status(self):
+        gear = self.data.read_packet.gear
+        speed = self.data.read_packet.speed / 10
+        steer = self.data.read_packet.steer / 71
+        brake = self.data.read_packet.brake / 200
+        return gear, speed, steer, brake
+
+
+if __name__ == "__main__":
+    test_data = Data()
+    test_platform = CarPlatform('COM6', test_data)
+    test_platform.main()
+    # TODO: 테스트 코드 만들고 테스트 해 보기
