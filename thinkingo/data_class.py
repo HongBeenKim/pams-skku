b1453a0fb48a9c7ce6119d3f33f70d24bcb7f9a1
Jueun <elpion19@gmail.com>
2018-10-29 21:51:01 +0900

    making communication code

--- thinkingo/data_class.py
+++ thinkingo/data_class.py
@@ -3,5 +3,21 @@ from serial_packet import SerialPacket
 
 class Data(object):
     def __init__(self):
-        self.read_packet = SerialPacket()
-        self.write_packet = SerialPacket()
+        self._read_packet = SerialPacket()
+        self._write_packet = SerialPacket()
+
+    @property
+    def read_packet(self):
+        return self._read_packet
+
+    @read_packet.setter
+    def read_packet(self, read_packet):
+        self._read_packet = read_packet
+
+    @property
+    def write_packet(self):
+        return self._write_packet
+
+    @write_packet.setter
+    def write_packet(self, write_packet):
+        self._write_packet = write_packet
