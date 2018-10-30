1e0a9ecef4e8577abc4e51947ec89f89ac5a4d6a
unknown <elpion19@gmail.com>
2018-10-31 00:26:56 +0900

    :tada: data logging tool set

---
+++ data_logging_set/save_log.py
@@ -0,0 +1,21 @@
+"""
+라이다와 캠 데이터를 동시에 저장하는 코드
+"""
+
+
+def main():
+    while True:
+        # 라이다, 캠 1, 2, 3 저장
+        pass
+
+
+def lidar():
+    pass
+
+
+def cam(number: int):
+    pass
+
+
+if __name__ == "__main__":
+    main()
