d76b99d20859c85df4ba0ed05b1fdfe336319d7c
HongBeenKim <dtb05045@gmail.com>
2018-11-11 05:36:24 +0900

    edit path

--- thinkingo/sign_cam.py
+++ thinkingo/sign_cam.py
@@ -2,9 +2,8 @@ import socket
 import msvcrt
 
 import sys
-import os
 
-sys.path.append(os.path.dirname(__file__))
+sys.path.append(".")
 from subroutine import Subroutine
 from data_class import Data
 
