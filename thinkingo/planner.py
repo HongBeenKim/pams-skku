bf2de3f41b624a022e94e13efde1e03a21442ba2
HongBeenKim <dtb05045@gmail.com>
2018-11-10 23:44:40 +0900

    Skeleton Code for "planner.py"

--- thinkingo/planner.py
+++ thinkingo/planner.py
@@ -1,5 +1,15 @@
 from subroutine import Subroutine
+from data_class import Data
 
 
 class MotionPlanner(Subroutine):
-    pass
+    def __init__(self, data: Data):
+        super().__init__(data)
+        # TODO: init할 것 생각하기
+
+    def main(self):
+        while True:
+            pass
+        # TODO: main함수 채우기
+
+    # TODO: 필요한 메서드 생각하기
