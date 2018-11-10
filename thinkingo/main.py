b0c4f3c5c51335b8f24817798d3d6e74f54ea3a3
Jueun-Park <elpion19@gmail.com>
2018-11-10 21:49:33 +0900

    :tada: add SignCam subroutine (it looks like goes well...)

--- thinkingo/main.py
+++ thinkingo/main.py
@@ -1,22 +1,27 @@
 import threading
+import sys
 
+sys.path.append(".")
 from data_class import Data
 
 from car_platform import CarPlatform
-from control import Control
 from lane_cam import LaneCam
 from lidar import Lidar
-from planner import MotionPlanner
 from sign_cam import SignCam
+from planner import MotionPlanner
+from control import Control
 
 
 def main():
     data = Data()
     platform = CarPlatform('COM6', data)
+    sign_cam = SignCam(data)
 
     platform_thread = threading.Thread(target=platform.main)
+    sign_cam_thread = threading.Thread(target=sign_cam.main)
 
     platform_thread.start()
+    sign_cam_thread.start()
 
 
 if __name__ == "__main__":
