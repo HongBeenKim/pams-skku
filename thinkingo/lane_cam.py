b067c9db11acb8eee0b8f4bf67292506c5cf245a
HongBeenKim <dtb05045@gmail.com>
2018-11-13 20:38:47 +0900

    'make_merged_frame' method is on progress

--- thinkingo/lane_cam.py
+++ thinkingo/lane_cam.py
@@ -16,9 +16,9 @@ class LaneCam(Subroutine):
     distortion_coefficients_R = np.array([0.116554, -0.155379, -0.001045, -0.001512])
 
     pts1_L = np.float32([[0, 0], [0, 428], [780, 0], [780, 428]])
-    pts2_L = np.float32([[0, 312], [440, 445], [537, 0], [562, 354]])
+    pts2_L = np.float32([[0, 258], [403, 427], [526, 0], [523, 347]])
     pts1_R = np.float32([[0, 0], [0, 428], [780, 0], [780, 428]])
-    pts2_R = np.float32([[5, 0], [-2, 384], [503, 324], [119, 471]])
+    pts2_R = np.float32([[0, 0], [15, 327], [459, 294], [130, 415]])
 
     Bird_view_matrix_L = cv2.getPerspectiveTransform(pts1_L, pts2_L)
     Bird_view_matrix_R = cv2.getPerspectiveTransform(pts1_R, pts2_R)
@@ -36,14 +36,16 @@ class LaneCam(Subroutine):
         left_frame = self.data_source.left_frame.copy()
         right_frame = self.data_source.right_frame.copy()
         undistorted_left = cv2.undistort(left_frame, self.camera_matrix_L,
-                                         self.distortion_coefficients_L,None, None)[10:438, 10:790]
+                                         self.distortion_coefficients_L, None, None)[10:438, 10:790]
         undistorted_right = cv2.undistort(right_frame, self.camera_matrix_R,
-                                         self.distortion_coefficients_R, None, None)[10:438, 10:790]
+                                          self.distortion_coefficients_R, None, None)[10:438, 10:790]
 
+        transformed_left = cv2.warpPerspective(undistorted_left, self.Bird_view_matrix_L, (526, 427))
+        transformed_right = cv2.warpPerspective(undistorted_right, self.Bird_view_matrix_R, (459, 415))
 
-
-
-        #TODO: warpPerspective
+        cv2.imshow('transformed left', transformed_left)
+        cv2.imshow('transformed right', transformed_right)
+        # TODO: warpPerspective
 
     def stop_line_detection(self):
         pass
@@ -65,19 +67,21 @@ class LaneCam(Subroutine):
                     if y1 == y2: continue
                     ceiling_interception = int(x1 - (0 - y1) * (x2 - x1) / (y1 - y2))
                     bottom_interception = int(x1 - (158 - y1) * (x2 - x1) / (y1 - y2))
-                    if 266 > ceiling_interception or ceiling_interception > 532 or x1 == x2 or (abs(y1 - y2) / abs(x1 - x2)) < 0.1 or np.sqrt(
+                    if 266 > ceiling_interception or ceiling_interception > 532 or x1 == x2 or (
+                            abs(y1 - y2) / abs(x1 - x2)) < 0.1 or np.sqrt(
                             (x1 - x2) ** 2 + (y1 - y2) ** 2) < 50: continue
                     cv2.circle(temp_frame, (int(x1 - (0 - y1) * (x2 - x1) / (y1 - y2)), 0), 10, 255, -1)
                     cv2.line(temp_frame, (x1, y1), (x2, y2), (0, 0, 255), 3)
 
-        #self.data.lane_value  # TODO: set lane values
+        # self.data.lane_value  # TODO: set lane values
 
         cv2.imshow('test', temp_frame)
         cv2.imshow('edged', edged)
 
     def main(self):
         while True:
-            self.lane_detection()
+            # self.lane_detection()
+            self.make_merged_frame()
             if cv2.waitKey(1) & 0xff == ord(' '): break
 
 
@@ -88,22 +92,21 @@ if __name__ == "__main__":
 
     testData = Data()
     testDS = Source()
-    testDDS = DummySource('2018-11-04-17-01-16')
-    testLC = LaneCam(testDDS, testData)  # DummySource for test
+    # testDDS = DummySource('2018-11-04-17-01-16')
+    testLC = LaneCam(testDS, testData)  # DummySource for test
 
     # lidar_source_thread = threading.Thread(target=testDS.lidar_stream_main)
-    # left_cam_source_thread = threading.Thread(target=testDS.left_cam_stream_main)
-    # right_cam_source_thread = threading.Thread(target=testDS.right_cam_stream_main)
+    left_cam_source_thread = threading.Thread(target=testDS.left_cam_stream_main)
+    right_cam_source_thread = threading.Thread(target=testDS.right_cam_stream_main)
     # mid_cam_source_thread = threading.Thread(target=testDS.mid_cam_stream_main)
 
-    dummy_thread = threading.Thread(target=testDDS.main)
-    dummy_thread.start()
+    # dummy_thread = threading.Thread(target=testDDS.main)
+    # dummy_thread.start()
 
     # lidar_source_thread.start()
-    # left_cam_source_thread.start()
-    # right_cam_source_thread.start()
+    left_cam_source_thread.start()
+    right_cam_source_thread.start()
     # mid_cam_source_thread.start()
 
     time.sleep(2)
-
     testLC.main()
